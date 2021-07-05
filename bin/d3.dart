8a4874c8edac67d980fd707de6f6e4ed2991fc7c
iceman951 <47938031+iceman951@users.noreply.github.com>
2021-07-05 19:33:57 +0700

    state on off

--- bin/d3.dart
+++ bin/d3.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 import 'dart:io';
 
 import 'package:d3/d3.dart' as d3;
@@ -5,6 +6,30 @@ import 'package:d3/d3.dart' as d3;
 abstract class State {
   void handler(Stateful context);
   String toString();
+}
+
+class StatusOff implements State {
+  handler(Stateful context) {
+    print(" ");
+    context.state = StatusOn();
+  }
+
+  @override
+  String toString() {
+    return "off";
+  }
+}
+
+class StatusOn implements State {
+  handler(Stateful context) {
+    print("");
+    context.state = StatusOff();
+  }
+
+  @override
+  String toString() {
+    return "on";
+  }
 }
 
 class Stateful {
@@ -15,17 +40,21 @@ class Stateful {
   State get state => _state;
   set state(State newState) => _state = newState;
 
-  void touch() {
-    print("  Touching the Stateful...");
+  void set(Float hours, Float minutes) {
+    print("settttt");
+    _state.handler(this);
+  }
+
+  void increaseMinutes(Float hours, Float minutes) {
+    print("set_clock");
     _state.handler(this);
   }
 }
 
 void main(List<String> arguments) {
+  var clockState = Stateful(StatusOff());
   print("time");
   print('command: ');
-  var line = stdin.readLineSync();
-  List<String> commandList = line.toString().split(' ');
   while (true) {
     print('command: ');
     var line = stdin.readLineSync();
