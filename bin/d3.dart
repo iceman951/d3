f921af6369a2e479e19d2a642bc01aca77892c7c
iceman951 <47938031+iceman951@users.noreply.github.com>
2021-07-05 19:17:37 +0700

    add input

--- bin/d3.dart
+++ bin/d3.dart
@@ -2,15 +2,33 @@ import 'dart:io';
 
 import 'package:d3/d3.dart' as d3;
 
-class Stateful {}
-
 abstract class State {
   void handler(Stateful context);
-  @override
   String toString();
 }
 
+class Stateful {
+  State _state;
+
+  Stateful(this._state);
+
+  State get state => _state;
+  set state(State newState) => _state = newState;
+
+  void touch() {
+    print("  Touching the Stateful...");
+    _state.handler(this);
+  }
+}
+
 void main(List<String> arguments) {
+  print("time");
+  print('command: ');
   var line = stdin.readLineSync();
-  List<String> lineList = line.toString().split(' ');
+  List<String> commandList = line.toString().split(' ');
+  while (true) {
+    print('command: ');
+    var line = stdin.readLineSync();
+    List<String> commandList = line.toString().split(' ');
+  }
 }
